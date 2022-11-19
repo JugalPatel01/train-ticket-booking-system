318a2cc8157c0824991c0a98326121b1217e7781
jugal_patel <jugalrpatel1704@gmail.com>
2022-11-20 01:41:39 +0530

    Added authentication,trains,schedules
    - authentication is fully functoinal
    - made scaffold of the trains and schedules

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  resources :schedules
+  resources :trains
+  devise_for :users
+  # get 'home/index'
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
 
   # Defines the root path route ("/")
   # root "articles#index"
